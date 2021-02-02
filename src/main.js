b17adb048e28441786cfe79aa46d65cacf65033a
vite <magisterium.vs@list.ru>
2021-02-02 13:08:36 +0300

    added index.html in root package for debug

---
+++ src/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+document.onreadystatechange = function () {
+
+    if (document.readyState !== "complete")
+        return;
+
+	var reader;
+	var storage = new EPUBJS.storage();
+
+	storage.init(function () {
+
+		storage.get(function (data) {
+
+			if (data !== undefined) {
+
+				reader = ePubReader(data, { restore: true });
+
+			} else {
+
+				ePubReader('https://s3.amazonaws.com/moby-dick/', {
+
+					restore: true
+				});
+			}
+		});
+	});
+
+    window.reader = reader;
+};
