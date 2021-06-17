4ef91b64de50798ae6da5fb6c00c20e7bac8e9aa
vite <magisterium.vs@list.ru>
2021-06-17 20:05:09 +0300

    added a simple search throughout the contents of the book

--- src/controllers/panels/search_panel.js
+++ src/controllers/panels/search_panel.js
@@ -5,17 +5,76 @@ export class SearchPanel {
     constructor(reader) {
 
         this.panel = new UIPanel().setId('search');
-        const ctrl = new UIRow();
+        this.query = undefined;
+        this.pages = [];
         this.searchBox = new UIInput('search');
         this.searchBox.dom.placeholder = 'search';
+        this.searchBox.dom.addEventListener('search', () => {
+
+            const value = this.searchBox.getValue();
+            
+            if (value.length === 0) {
+                this.clear();
+                this.query = value;
+            } else if (this.query !== value) {
+                this.clear();
+                this.query = value;
+                this.doSearch(value).then(results => {
+
+                    results.forEach(item => {
+                        this.set(item);
+                    });
+                });
+            }
+        });
         this.searchResult = document.createElement('ul');
 
-        ctrl.add(this.searchBox);
+        const ctrlRow = new UIRow();
+        ctrlRow.add(this.searchBox);
 
-        this.panel.add(ctrl);
-        this.panel.dom.appendChild(this.searchResult)
+        this.panel.add(ctrlRow);
+        this.panel.dom.appendChild(this.searchResult);
+        this.reader = reader;
         //
-        // no implemented
+        // improvement of the highlighting of keywords is required...
         //
+    }
+
+    /**
+     * Searching the entire book
+     * @param {*} q Query keyword
+     * @returns The search result array.
+     */
+    async doSearch(q) {
+
+        const book = this.reader.book;
+        const results = await Promise.all(
+            book.spine.spineItems.map(item => item.load(book.load.bind(book))
+            .then(item.find.bind(item, q)).finally(item.unload.bind(item))));
+        return await Promise.resolve([].concat.apply([], results));
+    }
+
+    set(data) {
+
+        const item = document.createElement('li');
+        const link = document.createElement('a');
+
+        link.href = "#" + data.cfi;
+        link.textContent = data.excerpt;
+        link.onclick = () => {
+
+            this.reader.rendition.display(data.cfi);
+            return false;
+        };
+
+        item.appendChild(link);
+        this.searchResult.appendChild(item);
+    }
+
+    clear() {
+
+        while (this.searchResult.hasChildNodes()) {
+            this.searchResult.removeChild(this.searchResult.lastChild);
+        }
     }
 }
